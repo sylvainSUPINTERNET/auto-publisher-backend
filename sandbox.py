2d54602cabfcf7232ad92177f1b352bd9a1cdfb4
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-11-27 23:38:36 +0100

    o

--- sandbox.py
+++ sandbox.py
@@ -13,13 +13,29 @@ clip = (
 ).resized(height=720)
 
 
-text_clip = (TextClip(font="Atop-R99O3.ttf", text="Hello World", font_size=70, color='white', font_size="caption")).with_position(("center", "top")).with_duration(clip.duration)
+text_clip = (TextClip(font="Atop-R99O3.ttf", text="Hello World", font_size=56, color='white')).with_position(("center", "top")).with_duration(clip.duration)
 
 new_video = CompositeVideoClip([clip, text_clip])
 
-# new_video.write_videofile("output.mp4", ffmpeg_params=["-preset", "ultrafast"] ) # use dogshit params ( consume CPU extremly !)
-#                           #codec="libx264", fps=24)
+# shit laptop
+# new_video.write_videofile("output_tiktok_60fps.mp4", codec="libx264", audio_codec="aac", 
+#                      ffmpeg_params=["-r", "60", "-vf", "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920", 
+#                                     "-preset", "ultrafast", "-b:v", "1500k"]) # , "-threads", "4"
 
-new_video.write_videofile("output_tiktok_60fps.mp4", codec="libx264", audio_codec="aac", 
-                     ffmpeg_params=["-r", "60", "-vf", "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920", 
-                                    "-preset", "ultrafast", "-b:v", "1500k"]) # , "-threads", "4"
+
+new_video.write_videofile(
+    "output_tiktok_60fps.mp4",
+    codec="libx264",
+    audio_codec="aac",
+    ffmpeg_params=[
+        "-r", "60",  # Fréquence d'images
+        "-vf", "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920",  # Mise à l'échelle
+        "-preset", "medium",  # Qualité d'encodage
+        "-b:v", "4000k",  # Bitrate vidéo
+        "-crf", "23",  # Contrôle de qualité
+        "-movflags", "faststart",  # Optimisation pour le streaming
+        "-profile:v", "high", "-level", "4.2",  # Encodage avancé
+        "-b:a", "192k"  # Bitrate audio
+    ]
+)
+
